92330b318d1ef9f72884314f9912296f397ef868
Catur Sugi Ade Kurniawan <94914489+Chaca024@users.noreply.github.com>
2023-06-20 00:23:17 +0700

    Add files via upload

---
+++ projekInventarisObat/src/projekinventarisobat/ProjekInventarisObat.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projekinventarisobat;
+
+import java.awt.BorderLayout;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Vector;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+
+/**
+ *
+ * @author USER
+ */
+public class ProjekInventarisObat {
+
+//    /**
+//     * @param args the command line arguments
+//     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
